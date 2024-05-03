57876cda4c2a324cfd7afa486040933bc6a50cfd
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-03 19:20:51 -0400

    Updated org

--- terraform/3_compute/data.tf
+++ terraform/3_compute/data.tf
@@ -5,7 +5,7 @@
 data "terraform_remote_state" "base_workspace" {
   backend = "remote"
   config = {
-    organization = "hashicorp"
+    organization = "ezrielasitgets"
     workspaces = {
       name = "${var.project_id}-1_base-${var.aws_region_short_names[var.aws_region]}"
     }
