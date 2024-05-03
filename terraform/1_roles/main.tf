53dd72b0c0b31c28b553e8d307ce90c98625efb6
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-03 15:02:28 -0400

    Fleshed out roles module

--- terraform/1_roles/main.tf
+++ terraform/1_roles/main.tf
@@ -1,13 +1,16 @@
+# -----------------------------------------------------------------------
+# Main
+# -----------------------------------------------------------------------
+
 module "iam_assumable_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
 
   create_role = true
 
-  role_name = "test"
+  role_name = "${var.project_id}-ecs-role-${var.aws_region_short_names[var.aws_Region]}"
 
   custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonCognitoReadOnly",
-    "arn:aws:iam::aws:policy/AlexaForBusinessFullAccess",
+    "arn:aws:iam::aws:policy/AmazonECS_FullAccess",
   ]
-  number_of_custom_role_policy_arns = 2
+  number_of_custom_role_policy_arns = 1
 }
