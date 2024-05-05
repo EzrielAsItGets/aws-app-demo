9437109e884d505c3469fb08d329a7338cc524e3
Ezriel Ciriaco <ej.ciriaco@gmail.com>
2024-05-05 13:16:04 -0400

    Added alb

--- terraform/1_base/outputs.tf
+++ terraform/1_base/outputs.tf
@@ -6,6 +6,14 @@ output "vpc_private_subnets" {
   value = module.vpc.private_subnets
 }
 
+output "vpc_public_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
 output "ecr_name" {
   value = module.ecr.repository_name
 }
